8eff7d54afbdebad846ed28e592e415d3998e3f5
Pipi-Olo <Copy.H13@gmail.com>
2021-12-01 02:45:57 +0900

    Initial #4 - EntityRelationApplication

---
+++ Back-End/Spring Data JPA/entity-relation/src/test/java/com/pipiolo/relation/EntityRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipiolo.relation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityRelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
