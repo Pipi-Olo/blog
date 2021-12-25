c3d4267f9b98862d46be0e6852eb99623bf24251
Pipi-Olo <Copy.H13@gmail.com>
2021-12-25 23:05:07 +0900

    Initial #7 - Spring for Kafka Model Message Application

---
+++ Back-End/Spring Kafka/spring-kafka-model-message/src/main/java/com/pipiolo/kafka/SpringKafkaModelMessageApplication.java
@@ -0,0 +1,13 @@
+package com.pipiolo.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaModelMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaModelMessageApplication.class, args);
+    }
+
+}
---
+++ Back-End/Spring Kafka/spring-kafka-model-message/src/test/java/com/pipiolo/kafka/SpringKafkaModelMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipiolo.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaModelMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
